437c73ebd26b38b5bd2cbefcbb6dad7a4f348817
Takayuki Niinuma <takayuki.niinuma@monstar-lab.com>
2015-12-20 08:52:52 +0900

    mod mobingi-install.sh

--- mobingi-install.sh
+++ mobingi-install.sh
@@ -1,11 +1,2 @@
-sudo su
-
-groupadd app
-useradd niinuma
-gpasswd -a niinuma app
-
-apt-get -y install git tree dstat sysstat vim
-apt-get -y install perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https awslogs
-
-sudo service awslogs start
-update-rc.d awslogs defaults
+sudo apt-get update
+sudo apt-get -y install curl
