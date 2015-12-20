56fd95fecb6b5df054335e02b2088cdbd654686a
Takayuki Niinuma <takayuki.niinuma@monstar-lab.com>
2015-12-21 06:11:54 +0900

    add newrelic setting

--- mobingi-install.sh
+++ mobingi-install.sh
@@ -1,2 +1,9 @@
 sudo apt-get update
 sudo apt-get -y install curl mlocate vim
+
+# New Relic Setting
+wget -O - https://download.newrelic.com/548C16BF.gpg | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.newrelic.com/debian/ newrelic non-free"
+> /etc/apt/sources.list.d/newrelic.list'
+sudo apt-get -y install newrelic-php5
+sudo newrelic-install install
