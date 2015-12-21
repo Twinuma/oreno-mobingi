076409a071de45f5e7e64d22912ca06732be1ee2
Takayuki Niinuma <takayuki.niinuma@monstar-lab.com>
2015-12-22 02:26:52 +0900

    add sendgrid sample

--- mobingi-install.sh
+++ mobingi-install.sh
@@ -1,5 +1,9 @@
 sudo apt-get update
-sudo apt-get install -y vim mlocate
+sudo apt-get install -y vim mlocate curl
+sudo apt-get install -y php5 libapache2-mod-php5
 
 cd /usr/share/nginx/www
-composer install
+sudo curl -sS https://getcomposer.org/installer | sudo php
+sudo mv composer.phar /usr/local/bin/composer
+
+sudo composer install
